2dce41f7a2feb2b6667904325940dfb25d6a6c9a
aaron seng <seng.aaron@gmail.com>
2023-01-13 11:21:17 +0300

    fix(ctor): add default ctor for the entities

--- Shared/Entities.cs
+++ Shared/Entities.cs
@@ -5,11 +5,22 @@
         public float Health;
         public float Power;
         public string Version = "A";
+
+        public Monster()
+        {
+            Health = 0;
+            Power = 0;
+        }
     }
 
     public struct User
     {
         public string Name;
         public string Group = "A";
+
+        public User()
+        {
+            Name = string.Empty;
+        }
     }
 }
